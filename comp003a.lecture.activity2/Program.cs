92d6912f7496620672e6c3796d5c7d54b281a6c3
Your Name <you@example.com>
2025-01-21 22:37:35 -0800

    Add project files.

---
+++ comp003a.lecture.activity2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Author:Roman 
+//Course:Comp-003a
+//Faculty: Jonathan 
+//Purpose:Lecture activity
+namespace comp003a.lecture.activity2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
